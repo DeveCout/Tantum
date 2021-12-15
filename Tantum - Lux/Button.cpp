da6589d9b47f39f5b402d9ba6a65a888567f92d8
DeveCout <devecout@gmail.com>
2021-12-15 15:04:25 +0100

    ✌ V0.1 Menu classes

--- Tantum - Lux/Button.cpp
+++ Tantum - Lux/Button.cpp
@@ -1,1 +1,100 @@
 #include "Button.h"
+#include <iostream>
+
+extern sf::Font fontClassic;
+
+Button::Button(sf::Vector2f const& pos, sf::Vector2f const& size, e_button_action const& action_,std::string const& content, MenuComponentStyle const& MenuComponentStyle_) :
+	buttonStyle(MenuComponentStyle_),buttonStyleOver(MenuComponentStyle(MenuComponentStyle_)),buttonStyleClick(MenuComponentStyle(MenuComponentStyle_)),
+	action(action_)
+{
+	hitbox = sf::RectangleShape(size);
+	
+	hitbox.setFillColor(buttonStyle.background);
+	hitbox.setPosition(pos);
+
+	sf::Vector2f hitboxCenter = hitbox.getPosition() + sf::Vector2f(hitbox.getLocalBounds().width/2, hitbox.getLocalBounds().height/2);
+	text = sf::Text(content,fontClassic);
+
+	text.setPosition(hitboxCenter - sf::Vector2f(text.getLocalBounds().width/2, text.getLocalBounds().height / 2));
+}
+
+e_button_action Button::click(sf::Vector2f const& posClick) const
+{
+	if (posClick.x > hitbox.getPosition().x && posClick.x < hitbox.getPosition().x + hitbox.getSize().x)
+	{
+		if (posClick.y > hitbox.getPosition().y && posClick.y < hitbox.getPosition().y + hitbox.getSize().y)
+		{
+			return action;
+		}
+	}
+	return NO_ACTION;
+}
+
+void Button::update(sf::Time const& dt, sf::Vector2f mousePos)
+{
+	if (click(mousePos) != NO_ACTION)
+	{
+		if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+		{
+			state = CLICK;
+			hitbox.setFillColor(buttonStyleClick.background);
+		}
+		else
+		{
+			state = OVER;
+			hitbox.setFillColor(buttonStyleOver.background);
+		}
+	}
+	else
+	{
+		state = DEFAULT;
+		hitbox.setFillColor(buttonStyle.background);
+	}
+}
+
+void Button::draw(sf::RenderTarget& target, sf::RenderStates states) const
+{
+	target.draw(hitbox);
+	if (state == OVER || state == CLICK)
+	{
+		sf::VertexArray lighting(sf::PrimitiveType::Quads, 4);
+		lighting[0].position = hitbox.getPoint(0) + hitbox.getPosition();
+		lighting[1].position = hitbox.getPoint(1) + hitbox.getPosition();
+		lighting[2].position = hitbox.getPoint(2) + hitbox.getPosition();
+		lighting[3].position = hitbox.getPoint(3) + hitbox.getPosition();
+
+
+		lighting[0].color = sf::Color(0, 0, 0, 0);
+		lighting[1].color = sf::Color(0, 0, 0, 0);
+		lighting[2].color = sf::Color(255, 255, 255, 200);
+		lighting[3].color = sf::Color(255, 255, 255, 200);
+
+		target.draw(lighting);
+	}
+	target.draw(text);
+}
+
+void Button::setColorText(sf::Color const& color)
+{
+	text.setFillColor(color);
+}
+
+void Button::setMenuComponentStyle(MenuComponentStyle const& style)
+{
+	buttonStyle = style;
+}
+
+void Button::setMenuComponentStyleOver(MenuComponentStyle const& style)
+{
+	buttonStyleOver = style;
+}
+
+void Button::setMenuComponentStyleClick(MenuComponentStyle const& style)
+{
+	buttonStyleClick = style;
+}
+
+void Button::setContent(std::string const& string)
+{
+	text.setString(string);
+}
--- Tantum - Lux/Menu.cpp
+++ Tantum - Lux/Menu.cpp
@@ -1,1 +1,120 @@
 #include "Menu.h"
+#include <iostream>
+
+
+Menu::Menu(e_menu_type const& menu):id(menu)
+{
+	buttons = std::vector<Button>();
+	tempAction = NO_ACTION;
+	sf::Color blue = sf::Color(0,235,150);
+	action = NO_ACTION;
+	switch (id)
+	{
+		case MAIN_MENU:
+		{
+			Button play = Button(sf::Vector2f(-250, -50), sf::Vector2f(500, 100), START, "PLAY", MenuComponentStyle(blue));
+			Button opt = Button(sf::Vector2f(-250, 100), sf::Vector2f(500, 100), GO_OPTION, "OPTION", MenuComponentStyle(sf::Color(50, 50, 50)));
+			Button quit = Button(sf::Vector2f(-250, 300), sf::Vector2f(500, 100), QUIT_GAME, "QUIT", MenuComponentStyle(sf::Color(50, 50, 50)));
+			
+			opt.setColorText(blue);
+			quit.setColorText(blue);
+
+			buttons.push_back(play);
+			buttons.push_back(opt);
+			buttons.push_back(quit);
+			break;
+		}
+		case OPTION:
+		{
+			Button audio = Button(sf::Vector2f(-250, -50), sf::Vector2f(500, 100), GO_AUDIO,"AUDIO", MenuComponentStyle(sf::Color(50, 50, 50)));
+			Button mainMenu = Button(sf::Vector2f(-250, 300), sf::Vector2f(500, 100), GO_MAIN_MENU, "RETURN", MenuComponentStyle(sf::Color(50, 50, 50)));
+			
+			mainMenu.setColorText(blue);
+			
+			buttons.push_back(audio);
+			buttons.push_back(mainMenu);			
+			break;
+		}
+		case AUDIO:
+		{
+			Info vol = Info(sf::Vector2f(-250, -50), sf::Vector2f(500, 100), "SoundManager::getVol()", MenuComponentStyle(sf::Color(50,50,50)));
+
+
+			Button volumeDown = Button(sf::Vector2f(-370, -50), sf::Vector2f(100, 100), VOLUME_UP, "-", MenuComponentStyle(sf::Color(50, 50, 50)));
+			Button volumeUp = Button(sf::Vector2f(270, -50), sf::Vector2f(100, 100), VOLUME_DOWN, "+", MenuComponentStyle(sf::Color(50, 50, 50)));
+			Button opt = Button(sf::Vector2f(-250, 300), sf::Vector2f(500, 100), GO_OPTION, "RETURN", MenuComponentStyle(sf::Color(50, 50, 50)));
+
+			opt.setColorText(blue);
+
+			buttons.push_back(opt);
+			buttons.push_back(volumeUp);
+			buttons.push_back(volumeDown);
+
+			infos.push_back(vol);
+
+			break;
+		}
+		case PAUSE:
+		{
+
+			break;
+		}
+	}
+}
+
+void Menu::update(sf::Time const& dt, sf::Vector2f const& mousePos)
+{
+	for (size_t i = 0; i < buttons.size(); i++)
+	{
+		buttons[i].update(dt, mousePos);
+	}
+
+	action = NO_ACTION;
+
+	//FLAG CLICK
+	if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+	{
+		if (clickFlag) { click = true; clickFlag = false; }
+		else { click = false; }
+	}else{click = false; clickFlag = true;}
+	//FLAG UNCLICK
+	if (!sf::Mouse::isButtonPressed(sf::Mouse::Left))
+	{
+		if (unclickFlag) { unclick = true; unclickFlag = false; }
+		else { unclick = false; }
+	}
+	else { unclick = false; unclickFlag = true; }
+
+
+	if (click)
+		for (size_t i = 0; i < buttons.size(); i++)
+		{
+			if (buttons[i].click(mousePos))
+				tempAction = buttons[i].click(mousePos);
+		}
+	if (unclick)
+	{
+		for (size_t i = 0; i < buttons.size(); i++)
+		{
+			if (buttons[i].click(mousePos) == tempAction)
+				action = tempAction;
+		}
+	}
+}
+
+void Menu::draw(sf::RenderTarget& target, sf::RenderStates states) const
+{
+	for (size_t i = 0; i < buttons.size(); i++)
+	{
+		target.draw(buttons[i]);
+	}
+	for (size_t i = 0; i < infos.size(); i++)
+	{
+		target.draw(infos[i]);
+	}
+}
+
+e_button_action Menu::getAction() const
+{
+	return action;
+}z
